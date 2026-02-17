cd3ee02add2ff8ffddb74f4d6cc4680a7830aea8
T12ys <155729923+T12ys@users.noreply.github.com>
2026-02-17 22:33:54 +0400

    Remove old migrations

--- obj/Debug/net8.0/HotelWebApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelWebApplication.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088ea7d1d36ffd736c5e67f8e7e0fdfa4ca30bdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ffb8c420fbea4157147f1f0102238738305798")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
