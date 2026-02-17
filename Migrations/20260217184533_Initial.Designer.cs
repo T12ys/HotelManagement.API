caa73199db68627557ca7cfb31b0e4c3a145056e
T12ys <155729923+T12ys@users.noreply.github.com>
2026-02-17 22:49:07 +0400

    refresh database

--- Migrations/20260217184533_Initial.Designer.cs
+++ Migrations/20260217184533_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelWebApplication.Migrations
 {
     [DbContext(typeof(HotelDbContext))]
-    [Migration("20260217183535_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20260217184533_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
